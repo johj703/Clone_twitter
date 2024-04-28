d523e675061ad92990db3e3be83ad5c2747bcf7a
Jo <johj703@nate.com>
2024-04-29 00:55:56 +0900

    onCreateAccountClick 오타 수정 및 부모 <div> 태그 추가

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "78640af3736f7b87"; }
+/******/ 		__webpack_require__.h = function() { return "f60fcf86d448cb83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,35 +15,37 @@ const Enter: NextPage = () => {
     reset();
     setMethod("login");
   };
-  const onCreateAcctountClick = () => {
+  const onCreateAccountClick = () => {
     reset();
     setMethod("create_account");
   };
   return (
-    <div className="flex flex-col items-center">
-      <div className="grid w-full grid-cols-2 mt-8 border-b">
-        <button
-          className={cls(
-            "pb-4 font-medium text-sm border-b-2",
-            method === "login"
-              ? "border-sky-500 text-sky-500"
-              : "border-transparent hover:text-gray-400 text-gray-500"
-          )}
-          onClick={onLoginClick}
-        >
-          로그인
-        </button>
-        <button
-          className={cls(
-            "pb-4 font-medium text-sm border-b-2",
-            method === "create_account"
-              ? "border-sky-500 text-sky-500"
-              : "border-transparent hover:text-gray-400 text-gray-500"
-          )}
-          onClick={onCreateAcctountClick}
-        >
-          회원가입
-        </button>
+    <div className="px-4 mt-16">
+      <div className="flex flex-col items-center">
+        <div className="grid w-full grid-cols-2 mt-8 border-b">
+          <button
+            className={cls(
+              "pb-4 font-medium text-sm border-b-2",
+              method === "login"
+                ? "border-sky-500 text-sky-500"
+                : "border-transparent hover:text-gray-400 text-gray-500"
+            )}
+            onClick={onLoginClick}
+          >
+            로그인
+          </button>
+          <button
+            className={cls(
+              "pb-4 font-medium text-sm border-b-2",
+              method === "create_account"
+                ? "border-sky-500 text-sky-500"
+                : "border-transparent hover:text-gray-400 text-gray-500"
+            )}
+            onClick={onCreateAccountClick}
+          >
+            회원가입
+          </button>
+        </div>
       </div>
     </div>
   );
